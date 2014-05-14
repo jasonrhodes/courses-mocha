d156c404544eec639307f00375f65d45181c3c7f
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-05-14 19:48:04 -0400

    Chai finished, preparing for quick fixtures lesson

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,26 +1,48 @@
 var getPalette = require("../lib/getPalette");
-var assert = require("assert");
+var expect = require("chai").expect;
+var fs = require("fs");
+
+var configFile = process.cwd() + "/config.json";
+
+function writeConfig(config, callback) {
+  fs.writeFile(configFile, JSON.stringify(config), callback);
+}
 
 describe("getPalette", function () {
 
-  it("should throw an error if the result is not an array", function () {
+  var config = {};
 
-    function fetch() {
-      return "not array";
-    }
+  before(function (done) {
 
-    assert.throws(function () {
-      getPalette(fetch);
-    }, /is not an array/);
+    fs.readFile(configFile, function (err, contents) {
+      config = JSON.parse(contents.toString());
+      done();
+    });
+
+  });
+
+  afterEach(function (done) {
+
+    writeConfig(config, done);
+
+  });
+
+  it("should throw an error if the result is not an array", function (done) {
+
+    writeConfig({ palette: "string" }, function (err) {
+
+      expect(getPalette).to.throw(/is not an array/);
+      done();
+
+    });
 
   });
 
   it("should return an array with 3 items by default", function () {
 
     var palette = getPalette();
-    assert(Array.isArray(palette), "did not return an array");
-    assert.equal(palette.length, 3, "did not return 3 items");
+    expect(palette).to.be.an("array").with.length(3);
 
-  })
+  });
 
 });
--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -1,12 +1,12 @@
 var hex2rgb = require("../lib/hex2rgb");
-var assert = require("assert");
+var expect = require("chai").expect;
 
 describe("hex2rgb", function () {
 
   it("should return an error if the value is not a hex code", function (done) {
 
     hex2rgb("blue", function (error, result) {
-      assert(error);
+      expect(error).to.exist;
       done();
     });
 
@@ -15,12 +15,13 @@ describe("hex2rgb", function () {
   it("should return a correctly converted rgb value", function (done) {
 
     hex2rgb("#fff", function (error, result) {
-      assert.strictEqual(error, null);
-      assert.deepEqual(result, [255, 255, 255]);
+
+      expect(error).to.not.exist;
+      expect(result).to.deep.equal([255, 255, 255]);
 
       done();
     });
 
-  })
+  });
 
 });
